0d2af81f24a7b5aa249210ac49ce11f8343c2e6f
Сеник Андрей Александрович <andrejj.senik@vseinstrumenti.ru>
2022-07-01 00:08:45 +0300

    tf-2: prod & stage

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -14,7 +14,7 @@ provider "yandex" {
   zone                     = var.zone
 }
 module "app" {
-  source          = "./modules/app"
+  source          = "../modules/app"
   public_key_path = var.public_key_path
   app_disk_image  = var.app_disk_image
   subnet_id       = var.subnet_id
@@ -22,7 +22,7 @@ module "app" {
 }
 
 module "db" {
-  source          = "./modules/db"
+  source          = "../modules/db"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
   subnet_id       = var.subnet_id
