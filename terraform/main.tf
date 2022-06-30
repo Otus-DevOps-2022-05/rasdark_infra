fed4103644ca9bb76006808209ebbb453226ed2b
Сеник Андрей Александрович <andrejj.senik@vseinstrumenti.ru>
2022-06-30 22:41:01 +0300

    tf-2: begin

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,17 @@ data "yandex_compute_image" "reddit" {
   folder_id = var.folder_id
 }
 
+resource "yandex_vpc_network" "app-network" {
+  name = "reddit-app-network"
+}
+
+resource "yandex_vpc_subnet" "app-subnet" {
+  name           = "reddit-app-subnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.app-network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
 resource "yandex_compute_instance" "app" {
   name  = "reddit-app-${count.index}"
   count = var.instance_count
@@ -33,7 +44,7 @@ resource "yandex_compute_instance" "app" {
     }
   }
   network_interface {
-    subnet_id = var.subnet_id
+    subnet_id = yandex_vpc_subnet.app-subnet.id
     nat       = true
   }
   metadata = {
